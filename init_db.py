ad5105ee0ae722c94c4c5d0283962f09d1a97459
brothel2077 <90911100+brothel2077@users.noreply.github.com>
2024-05-03 13:17:43 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -10,11 +10,12 @@ def init_dba():
             
     cur = conn.cursor()
 
-    cur.execute('DROP TABLE IF EXISTS public.fio;')
+    cur.execute('DROP TABLE IF EXISTS public.namep;')
     cur.execute('CREATE TABLE public.fio (id serial PRIMARY KEY,'
-                                            'name varchar (50) NOT NULL,'
-                                            'date varchar (50) NOT NULL,'
-                                            'spec varchar (50) NOT NULL);'
+                                            'name-o varchar (50) NOT NULL,'
+                                            'name-p varchar (50) NOT NULL,'
+                                            'date-o varchar (50) NOT NULL,'
+                                            'date-p varchar (50) NOT NULL);'
                                             )
                                             
     cur.execute('DROP TABLE IF EXISTS public.user;')
@@ -22,47 +23,55 @@ def init_dba():
                                             'login varchar (90) NOT NULL,'
                                             'sms varchar (90) NOT NULL,'
                                             'id1 integer NOT NULL,'
-                                            'date varchar (50) NOT NULL);'
+                                            'chair integer NOT NULL,'
+                                            'vagon integer NOT NULL,'
                                             )
                                             
-    cur.execute('INSERT INTO public.fio (name, date, spec) VALUES (%s, %s, %s)',
-                ('Ходяков Иван Валерьевич',
-                '08:00-15:00',
-                'Стоматолог')
+    cur.execute('INSERT INTO public.fio (name-o, name-p, date-o, date-p) VALUES (%s, %s, %s, %s)',
+                ('Москва Ярославская',
+                'Петушки',
+                '15:17',
+                '18:22')
                 )
-    cur.execute('INSERT INTO public.fio (name, date, spec) VALUES (%s, %s, %s)',
-                ('Чечевский Андрей Сергеевич',
-                '09:00-18:00',
-                'Участковый врач')
+    cur.execute('INSERT INTO public.fio (name-o, name-p, date-o, date-p) VALUES (%s, %s, %s, %s)',
+                ('Москва Ленинградская',
+                'Санкт-Петербург',
+                '15:30',
+                '20:15')
                 )
-    cur.execute('INSERT INTO public.fio (name, date, spec) VALUES (%s, %s, %s)',
-                ('Ерулина Жанна Аркадьевна',
-                '12:00-16:00',
-                'Психолог')
+    cur.execute('INSERT INTO public.fio (name-o, name-p, date-o, date-p) VALUES (%s, %s, %s, %s)',
+                ('Ярославль',
+                'Москва Ярославская',
+                '16:10',
+                '20:10')
                 )
-    cur.execute('INSERT INTO public.fio (name, date, spec) VALUES (%s, %s, %s)',
-                ('Пастух Ольга Михайловна',
-                '07:00-10:00',
-                'Лаборант')
+    cur.execute('INSERT INTO public.fio (name-o, name-p, date-o, date-p) VALUES (%s, %s, %s, %s)',
+                ('Подольск',
+                'Нахабино',
+                '19:02',
+                '21:12')
                 )    
                            
-    cur.execute('INSERT INTO public.user (login, sms, id1, date) VALUES (%s, %s, %s, %s)',
-                ('+78986664502',
-                '5500',
+    cur.execute('INSERT INTO public.user (login, sms, id1, chair, vagon) VALUES (%s, %s, %s, %s, %s)',
+                ('Сложнов Егор Алексеевич',
+                '4518',
                 2,
-                '12:15')
+                '15',
+                6)
                 )
-    cur.execute('INSERT INTO public.user (login, sms, id1, date) VALUES (%s, %s, %s, %s)',
-                ('+79196663409',
-                '0208',
+    cur.execute('INSERT INTO public.user (login, sms, id1, chair, vagon) VALUES (%s, %s, %s, %s, %s)',
+                ('Панченков Илья Сергеевич',
+                '3917',
                 1,
-                '13:00')
+                '13',
+                7)
                 )
-    cur.execute('INSERT INTO public.user (login, sms, id1, date) VALUES (%s, %s, %s, %s)',
-                ('+79265340071',
-                '3648',
-                4,
-                '07:25')
+    cur.execute('INSERT INTO public.user (login, sms, id1, chair, vagon) VALUES (%s, %s, %s, %s, %s)',
+               ('Венедикт Васильевич Ерофеев',
+                '3879',
+                1,
+                '11',
+                7)
                 )
 
     conn.commit()
